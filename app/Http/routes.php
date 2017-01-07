44dca8a06021ea910a8b936c4b3875e3c4297c93
Andrei <AndreiTimotin11@gmail.com>
2017-01-07 17:14:55 +0200

    1-st commit

---
+++ app/Http/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/page/{id_cat}/{id}', function ($id) {
+     echo '<pre>';
+     echo $id;
+ //    print_r($_ENV);
+     echo config('app.locale');
+     echo Config::set('app.locale', 'ru');
+     echo Config::get('app.locale');
+     echo env('APP_ENV');
+     echo '<pre>';
+});
+Route::get('/comments', function (){
+    print_r($_POST);
+});
+//Route:get('/',function (){});
