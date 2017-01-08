e087bf83ca1f44c339d96dcedbbd90f57b2eaf55
Andrei <AndreiTimotin11@gmail.com>
2017-01-08 15:24:33 +0200

    Controllers

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use  App\Http\Controllers\Controller;
+
+class FirstController
+    extends Controller
+{
+    public function show($id){
+        echo $id;
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,27 +14,4 @@
 Route::get('/',['as'=>'home', function () {
     return view('welcome');
 }]);
-
-Route::get('/article/{id}',
-    ['as'=>'article',
-        function ($id) {
-   echo $id;
-}]);
-
-
-Route::get('/page/{id}', function ($id) {
-     echo '<pre>';
-     echo $id ;
-})->where('id', '[0-9]+');
-
-Route::get('/comments', function (){
-    print_r($_POST);
-});
-Route::group(['prefix'=>'admin'], function (){
-    Route::get('page/create', function (){
-        return redirect()->route('home');
-    });
-    Route::get('page/edit', function (){
-        echo 'page/edit';
-    });
-});
+Route::get('/about/{id}', 'FirstController@show');
