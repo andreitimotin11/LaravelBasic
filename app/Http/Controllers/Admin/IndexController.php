c24649e813887ebb291775fc9e7adcf4c8264c76
Andrei <AndreiTimotin11@gmail.com>
2017-01-11 19:51:06 +0200

    11. Work with views

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -13,6 +13,6 @@ class IndexController extends Controller
     public function show(){
         echo 1;
 
-        return view('default.template', [''=>'']);
+        return view('default.template', ['title'=>'Hello World!']);
     }
 }
